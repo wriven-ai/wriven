fddac868e580b1724358d8995b49c74541d63816
anowarzz <anowarhosen444@gmail.com>
2026-08-20 11:57:20 +0600

    feat(client): add vercel analytics

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next';
 import { cn } from '@/lib/utils';
 import { Manrope } from 'next/font/google';
 import './global.css';
@@ -23,6 +24,7 @@ export default function RootLayout({
     >
       <body>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
